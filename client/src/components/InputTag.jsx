63d4207adb28e09ac603b065dd5d71341dc165b8
monilprajapati <monillprajapatii@gmail.com>
2024-01-20 00:42:55 +0530

    Done with Auth frontend

--- client/src/components/InputTag.jsx
+++ client/src/components/InputTag.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FaEyeSlash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
 
-const InputTag = () => {
+const InputTag = ({ name, type, id, value, placeholder, icon }) => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <>
-    
+      <div className="relative w-full mb-4 flex items-center">
+        <div className="input-icon">
+          {(() => {
+            switch (icon) {
+              case "fullname":
+                return <FaUser className="text-gray-400" />;
+              case "email":
+                return <MdOutlineMailOutline className="text-gray-400" />;
+              case "password":
+                return <RiLockPasswordFill className="text-gray-400" />;
+              default:
+                return <FaUser className="text-gray-400" />;
+            }
+          })()}
+        </div>
+        <input
+          id={id}
+          name={name}
+          type={
+            type == "password" ? (showPassword ? "text" : "password") : type
+          }
+          defaultValue={value}
+          placeholder={placeholder}
+          className="input-box"
+        />
+        {type == "password" ? (
+          <div
+            className="text-gray-400 input-icon left-[auto] right-3 p-3 cursor-pointer h-full flex items-center justify-center"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </div>
+        ) : null}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default InputTag
+export default InputTag;
--- client/src/pages/UserAuthForm.jsx
+++ client/src/pages/UserAuthForm.jsx
@@ -1,20 +1,88 @@
-import React from 'react'
-import InputTag from '../components/InputTag'
+import React from "react";
+import InputTag from "../components/InputTag";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import AnimationWrapper from "../common/AnimationWrapper";
 
-const UserAuthForm = ({type}) => {
+const UserAuthForm = ({ type }) => {
   return (
-    <section className='h-cover flex items-center justify-center'>
-        <form className='w-[80%] max-h-[400px]'>
-            <h1 className='text-4xl font-gelasio capitalize text-center mb-24'>
-                {type === 'sign-in' ? 'Welcome back' : 'Create an account'}
-            </h1>
-            {
-                type != "sing-in" ?
-                <InputTag/> : ""
-            }
-        </form>
-    </section>
-  )
-}
+    <AnimationWrapper keyValue={type}>
+      <section className="h-cover flex items-center justify-center">
+        <form className="w-[80%] max-h-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type === "sign-in" ? "Welcome back" : "Create an account"}
+          </h1>
+          {type != "sing-in" ? (
+            <InputTag
+              name="username"
+              type="text"
+              id="username"
+              value=""
+              placeholder="Username"
+              icon="email"
+            />
+          ) : (
+            ""
+          )}
+          <InputTag
+            name="email"
+            type="email"
+            id="email"
+            value=""
+            placeholder="Email"
+            icon="email"
+          />
+          <InputTag
+            name="password"
+            type="password"
+            id="password"
+            value=""
+            placeholder="Password"
+            icon="password"
+          />
+          <button className="btn-dark center mt-14">
+            {type.replace("-", " ")}
+          </button>
 
-export default UserAuthForm
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
+            <span className="text-center">or</span>
+            <hr className="w-1/2 border-black" />
+          </div>
+
+          {/* google button */}
+          <div className="btn-dark flex items-center justify-center gap-3">
+            <FcGoogle className="text-[18px]" />
+            {type.replace("-", " ")} with Google
+          </div>
+
+          <p className="mt-6 text-dark-grey text-xl text-center">
+            {type === "sign-in" ? (
+              <>
+                Don't have an account?
+                <Link
+                  to="/signup"
+                  className="underline text-black text-xl ml-1"
+                >
+                  Sign up
+                </Link>
+              </>
+            ) : (
+              <>
+                Already have an account?
+                <Link
+                  to="/signin"
+                  className="underline text-black text-xl ml-1"
+                >
+                  Sign in
+                </Link>
+              </>
+            )}
+          </p>
+        </form>
+      </section>
+    </AnimationWrapper>
+  );
+};
+
+export default UserAuthForm;
