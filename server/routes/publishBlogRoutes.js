3640b1b8992d0effd7202ecd3c66dde0b1b1fdab
monilprajapati <monillprajapatii@gmail.com>
2024-02-27 23:05:22 +0530

    Changes

--- server/routes/publishBlogRoutes.js
+++ server/routes/publishBlogRoutes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getUploadImageUrl } from "../controllers/blogController.js";
 
 const publishBlogRoutes = Router();
-// Here is the route for the getUploadImageUrl function
+
 publishBlogRoutes.get("/get-upload-url", getUploadImageUrl);
 
 export default publishBlogRoutes;
