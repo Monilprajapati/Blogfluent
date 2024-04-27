df7242eae38f9fcabdecea1984570f3f70619d5f
monilprajapati <monillprajapatii@gmail.com>
2024-04-27 16:25:51 +0530

    Done with the stuff

--- client/src/components/NoData.jsx
+++ client/src/components/NoData.jsx
@@ -3,9 +3,9 @@ import { LuInbox } from "react-icons/lu";
 
 const NoData = ({ message }) => {
   return (
-    <div className="text-center w-full p-4 rounded-full bg-grey/50 mt-4">
+    <div className="text-center w-full p-4 rounded-3xl bg-grey/50 mt-4">
       <div className="flex flex-col items-center justify-center gap-2 w-full">
-        <LuInbox className="text-5xl text-gray-300" />
+        <LuInbox className="text-5xl text-dark-grey" />
         <p>{message}</p>
       </div>
     </div>
